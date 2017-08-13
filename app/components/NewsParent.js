9801d6063da9d8bb13f0b69134ca078ede9812db
TimGaz92 <tgaziano@optonline.net>
2017-08-13 08:35:11 -0400

    still working on react components

--- app/components/NewsParent.js
+++ app/components/NewsParent.js
@@ -1,17 +1,13 @@
-/ Include React
 var React = require("react");
 
-// Here we include all of the sub-components
-var Child = require("./Child");
-
-// Requiring our helper for making API calls
+//sub components header & footer, header will change by news source 
+//footer is more or less static, but served to UI via react
+var Child = require("./Header");
 var helpers = require("../utils/helpers");
 
 // Create the Parent Component
 var Parent = React.createClass({
 
-  // Here we set a generic state associated with the number of clicks
-    //on load with no db call, sets values to 0
   getInitialState: function() {
     return {
       clicks: 0,
@@ -20,17 +16,12 @@ var Parent = React.createClass({
   },
 
   //  On load display the number of clicks
-    //on page load immmediately changes state of the page, pulls from db and updates relevant info 
   componentDidMount: function() {
     console.log("COMPONENT MOUNTED");
 
-    // The moment the page renders on page load, we will retrieve the previous click count.
-    // We will then utilize that click count to change the value of the click state.
     helpers.getClicks()
       .then(function(response) {
-        // Using a ternary operator we can set newClicks to the number of clicks in our response object
-        // If we don't have any clicks in our database, set newClicks to 0
-        var newClicks = response.data.length ? response.data[0].clicks : 0;
+        var newNews = response.data.length ? response.data[0].clicks : 0;
         this.setState({
           clicks: newClicks
         });
@@ -38,33 +29,26 @@ var Parent = React.createClass({
         console.log("Saved clicks", newClicks);
       }.bind(this));
   },
-  // Whenever our component updates, the code inside componentDidUpdate is run
+
+
   componentDidUpdate: function(prevState) {
     console.log("COMPONENT UPDATED");
-
-    // We will check if the click count has changed...
     if (prevState.clicks !== this.state.clicks) {
-
-      // If it does, then update the clickcount in MongoDB
       helpers.saveClicks({ clickID: this.state.clickID, clicks: this.state.clicks })
         .then(function() {
           console.log("Posted to MongoDB");
         });
     }
   },
-  // Whenever the button is clicked we'll use setState to add to the clickCounter
-  // Note the syntax for setting the state
+
   handleClick: function() {
     this.setState({ clicks: this.state.clicks + 1 });
   },
 
-  // Whenever the button is clicked we'll use setState to reset the clickCounter
-  // This will reset the clicks -- and it will be passed ALL children
   resetClick: function() {
     this.setState({ clicks: 0 });
   },
 
-  // Here we render the function
   render: function() {
     return (
       <div className="container">
@@ -72,17 +56,9 @@ var Parent = React.createClass({
         <div className="row">
 
           <div className="jumbotron">
-            <h2>All my children: A Tale of Reactive Components</h2>
+            <h2>React-ing to the News</h2>
             <hr />
             <p>
-              <em>Now backed by the power of MongoDB!</em>
-              Just visit <a href="/api">/api</a> to check out the DB!
-            </p>
-            <p>
-              {/*
-                Here we create a button click.
-                Note how we have an onClick event associate with our handleClick function.
-              */}
               <button
                 className="btn btn-primary btn-lg"
                 type="button"
@@ -90,7 +66,7 @@ var Parent = React.createClass({
               >
                 CLICK ME!!!!
               </button>
-              {/* Here we create a button click for resetting the clickCounter */}
+
               <button
                 className="btn btn-danger btn-lg"
                 type="button"
@@ -100,7 +76,7 @@ var Parent = React.createClass({
               </button>
             </p>
           </div>
-          {/* This represents the "Parent" */}
+
           <div className="col-md-12">
 
             <div className="panel panel-default">
@@ -109,13 +85,9 @@ var Parent = React.createClass({
               </div>
               <div className="panel-body text-center">
 
-                {/* This is where we'll show the click count for the parent */}
+
                 <h1>{this.state.clicks}</h1>
 
-                {/*
-                  Here we'll deploy the child component.
-                  We'll pass it the parent's click counter as a "state"
-                */}
                 <Child clicks={this.state.clicks} />
 
               </div>
